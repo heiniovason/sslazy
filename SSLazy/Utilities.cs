e58ef20ef7068a5a5d5e9f05cdf0ef29afc568f5
heiniovason <heini.ovason@gmail.com>
2020-05-23 19:51:24 +0200

    Initial commit

---
+++ SSLazy/Utilities.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net.Http;
+using System.Security.Authentication;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SSLazy
+{
+    public static class Utilities
+    {
+        public static X509Certificate2 GetCertificate(string thumbprint)
+        {
+            X509Certificate2 cert = null;
+            foreach (StoreLocation storeLocation in (StoreLocation[])
+                Enum.GetValues(typeof(StoreLocation)))
+            {
+                foreach (StoreName storeName in (StoreName[])
+                    Enum.GetValues(typeof(StoreName)))
+                {
+                    X509Store certStore = new X509Store(storeName, storeLocation);
+                    try
+                    {
+                        certStore.Open(OpenFlags.ReadOnly);
+                        X509Certificate2Collection certCollection = certStore.Certificates.Find(
+                            X509FindType.FindByThumbprint,
+                            thumbprint,
+                            false
+                            );
+                        certStore.Close();
+
+                        if (certCollection.Count > 0)
+                        {
+                            cert = certCollection[0];
+                        }
+                    }
+                    catch (CryptographicException ex)
+                    {
+                        Console.WriteLine(ex);
+                        // Log exception to ...?
+                    }
+                }
+            }
+            return cert;
+        }
+
+        public static HttpClient GetConfiguredHttpClient(X509Certificate2 certificate)
+        {
+            HttpClient httpClient = null;
+            var handler = new HttpClientHandler
+            {
+                ClientCertificateOptions = ClientCertificateOption.Manual,
+                SslProtocols = SslProtocols.Tls12
+            };
+            try
+            {
+                handler.ClientCertificates.Add(certificate);
+
+                httpClient = new HttpClient(handler);
+            }
+            catch (CryptographicException ex)
+            {
+                Console.WriteLine(ex);
+                // Log exception to ...?
+            }
+            return httpClient;
+        }
+    }
+}
---
+++ TryItOut/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using SSLazy;
+
+namespace TryItOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get and install PKCS12 from https://badssl.com/download/
+
+            string thumbprint = args[0];
+
+            // e.g. https://client.badssl.com/
+            string url = args[1];
+
+            X509Certificate2 certificate = Utilities.GetCertificate(thumbprint);
+
+            if (certificate != null)
+            {
+                string output = string.Format("Certificate issued by: {0}", certificate.Issuer);
+                Console.WriteLine(output);
+            }
+
+            Console.WriteLine("Configuring HttpClient ...");
+
+            HttpClient client = Utilities.GetConfiguredHttpClient(certificate);
+
+            Console.WriteLine("Calling server and waiting for response ...");
+
+            var response = client.GetAsync(url);
+
+            string result = response.Result.ToString();
+
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
